3cec9f06fa47b58a8bc069413b1597798a056b76
PilarPinto <piapintoch@unal.edu.co>
2020-07-15 18:25:50 +0000

    [Test]:Edges cases

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -2,12 +2,13 @@
 '''
 Take the shape of the matrix
 '''
+
+
 def matrix_shape(matrix):
     shape_m = []
     shape_m.append(len(matrix))
     if type(matrix[0]) == list:
-        shape_m.append(len(matrix[0]))
-        if type(matrix[0][0]) == list:
-            shape_m.append(len(matrix[0][0]))
-
+        while type(matrix[0]) != int:
+            shape_m.append(len(matrix[0]))
+            matrix = matrix[0]
     return shape_m
