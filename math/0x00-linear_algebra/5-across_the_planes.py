69cd05852675f971767ad50f14ccd4183d54294a
PilarPinto <piapintoch@unal.edu.co>
2020-07-15 21:09:30 +0000

    [Test]:Adding a float elements

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -9,7 +9,7 @@ def matrix_shape(matrix):
     shape_m = []
     shape_m.append(len(matrix))
     if type(matrix[0]) == list:
-        while type(matrix[0]) != int:
+        while (type(matrix[0]) != int and type(matrix[0]) != float):
             shape_m.append(len(matrix[0]))
             matrix = matrix[0]
     return shape_m
