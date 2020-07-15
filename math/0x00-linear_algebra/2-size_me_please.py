5374d41ba63cebf7415823a1347ba61400f9ca10
PilarPinto <piapintoch@unal.edu.co>
2020-07-15 18:31:22 +0000

    [Fix]-Adding definition comment

--- math/0x00-linear_algebra/2-size_me_please.py
+++ math/0x00-linear_algebra/2-size_me_please.py
@@ -5,6 +5,9 @@ Take the shape of the matrix
 
 
 def matrix_shape(matrix):
+    '''Matrix iterative function, in order to found the
+    shape of the matrix depends of the dimension'''
+
     shape_m = []
     shape_m.append(len(matrix))
     if type(matrix[0]) == list:
