4d26f7b29c9aecd11566e0e406c6064a7ebb0ec6
PilarPinto <piapintoch@unal.edu.co>
2020-07-16 20:50:32 +0000

    [Feature]: Change the conditionals

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -4,20 +4,9 @@ Script two add two matix by elements-wise
 '''
 
 
-def matrix_shape(matrix):
-    '''Function to obtain a shape of a matrix'''
-    shape_m = []
-    shape_m.append(len(matrix))
-    if type(matrix[0]) == list:
-        while (type(matrix[0]) != int and type(matrix[0]) != float):
-            shape_m.append(len(matrix[0]))
-            matrix = matrix[0]
-    return shape_m
-
-
 def add_matrices2D(mat1, mat2):
     '''Function to add two matrix by element wise'''
-    if matrix_shape(mat1) == matrix_shape(mat2):
+    if (len(mat1) == len(mat2)) and (len(mat1[0]) == len(mat2[0])):
         addi2mat = [[mat1[i][j] + mat2[i][j] for j in range(
             len(mat1[0]))] for i in range(len(mat1))]
         return addi2mat
