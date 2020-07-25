3320d1feb3e95c2473c46676f974b5a7ffaa54df
PilarPinto <piapintoch@unal.edu.co>
2020-07-25 04:10:20 +0000

    [Fix]:Changing a condition

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -5,7 +5,7 @@ import functools
 
 def summation_i_squared(n):
     '''Sum definition'''
-    if (isinstance(n, int) and n != 0):
+    if (isinstance(n, int) and n > 0):
         return(functools.reduce(lambda a, b: a+b, list(
             map(lambda i: i**2, range(n+1)))))
     else:
