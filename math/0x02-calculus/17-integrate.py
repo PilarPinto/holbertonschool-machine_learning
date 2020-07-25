e1d9503c61dbb0e8d05cbb86dd5f23669480aa29
PilarPinto <piapintoch@unal.edu.co>
2020-07-25 17:37:49 +0000

    [Fix]:Remove if instance

--- math/0x02-calculus/17-integrate.py
+++ math/0x02-calculus/17-integrate.py
@@ -8,15 +8,14 @@ def poly_integral(poly, C=0):
     if type(poly) is not list or len(poly) is 0 or type(C) is not int:
         return None
 
+    intg_list = [C]
+    
     if sum(poly) == 0:
-        return [C]
+        return intg_list
 
-    intg_list = []
-    intg_list.append(C)
-
-    if (isinstance(poly, list) and len(poly) > 0):
-        for index, item in enumerate(poly):
-            int_num = 1/(index+1)
-            num = int_num*item
-            intg_list.append(int(num) if num.is_integer() else num)
-        return(intg_list)
+    
+    for index, item in enumerate(poly):
+        int_num = 1/(index+1)
+        num = int_num*item
+        intg_list.append(int(num) if num.is_integer() else num)
+    return(intg_list)
