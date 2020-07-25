9e77e5176272c7db3a826e33d1cf84352d9fc913
PilarPinto <piapintoch@unal.edu.co>
2020-07-25 05:06:39 +0000

    [Fix]:Adding condition to derivate result

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -5,14 +5,16 @@
 def poly_derivative(poly):
     '''Polynomial derivate'''
     der_list = []
+
+    if (not isinstance(poly, list) and len(poly) == 0):
+        return None
+    
     if (len(poly) == 1):
         return('[0]')
 
-    if (isinstance(poly, list) and len(poly) != 0):
+    if (isinstance(poly, list) and len(poly) > 0):
         for index, item in enumerate(poly):
             num = index*item
             if index != 0:
                 der_list.append(num)
         return(der_list)
-    else:
-        return None
