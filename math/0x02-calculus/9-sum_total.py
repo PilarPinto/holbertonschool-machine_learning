24a94571299ad9aed52718dd527ac973d8453ba0
PilarPinto <piapintoch@unal.edu.co>
2020-07-25 04:06:02 +0000

    [Feature]: Sumatory python

---
+++ math/0x02-calculus/9-sum_total.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+'''Sum program'''
+import functools
+
+
+def summation_i_squared(n):
+    '''Sum definition'''
+    if (isinstance(n, int) and n != 0):
+        return(functools.reduce(lambda a, b: a+b, list(
+            map(lambda i: i**2, range(n+1)))))
+    else:
+        return None
