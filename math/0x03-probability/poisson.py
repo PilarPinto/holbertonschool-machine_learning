408979cd1463b8b69cb6aab77711e004ef4b243c
PilarPinto <piapintoch@unal.edu.co>
2020-07-29 23:12:37 +0000

    [Taking the mean of a set of data]

---
+++ math/0x03-probability/poisson.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+class Poisson:
+    '''
+    Class poisson find mean
+    '''
+    def __init__(self, data=None, lambtha=1.):
+
+        if data is None:
+            if lambtha < 0:
+                raise ValueError('lambtha must be a positive value')
+            else:
+                self.lambtha = float(lambtha)
+        else:
+            if not isinstance(data, list):
+                raise TypeError('data must be a list')
+            elif len(data) < 2:
+                raise ValueError('data must contain multiple values')
+            else:
+                lambtha = sum(data) / len(data)
+                self.lambtha = float(lambtha)
+
+    def pmf(self, k):
+        e = 2.71828
+        facto_k = 1
+        if (k % 1 != 0):
+            k = int(k)
+        if k <= 0:
+            return 0
+        for ind in range(1, k+1):
+            facto_k = facto_k * ind
+        pmf = ((e**-self.lambtha)*(self.lambtha**k))/facto_k
+        return pmf
