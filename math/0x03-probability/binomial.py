a192d5b23cad51e382c694c250cf2a17d733b483
PilarPinto <piapintoch@unal.edu.co>
2020-07-31 00:07:06 +0000

    [Test]:Adding condition

--- math/0x03-probability/binomial.py
+++ math/0x03-probability/binomial.py
@@ -11,7 +11,7 @@ class Binomial:
         if data is None:
             if n <= 0:
                 raise ValueError('n must be a positive value')
-            if p > 1 or p < 0:
+            if p >= 1 or p <= 0:
                 raise ValueError('p must be greater than 0 and less than 1')
             else:
                 self.n = int(n)
