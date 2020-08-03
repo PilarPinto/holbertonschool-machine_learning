4e783ee0f4bee67514894d7acdc7ed32a85bad64
PilarPinto <piapintoch@unal.edu.co>
2020-08-03 16:47:35 +0000

    [Feature]: Present a random weights based on a normal distribution

---
+++ supervised_learning/0x00-binary_classification/0-neuron.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+'''Neuron file'''
+
+import numpy as np
+
+
+class Neuron:
+    '''Class Neuron definition'''
+    def __init__(self, nx):
+        '''class constructor with features of the neuron'''
+
+        if nx % 1 != 0:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.W = np.random.normal(size=(1, nx))
+        self.b = 0
+        self.A = 0
