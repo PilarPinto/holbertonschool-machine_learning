63fb7ba70705b58a6d14d278a996928340aaa82e
PilarPinto <piapintoch@unal.edu.co>
2020-08-04 19:42:48 +0000

    [Feature]:Neuron cost

---
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+'''Neuron file'''
+
+import numpy as np
+
+
+class Neuron:
+    '''Class Neuron definition'''
+    def __init__(self, nx):
+        '''class constructor with features of the neuron'''
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+
+        self.__W = np.random.normal(size=(1, nx))
+        self.__b = 0
+        self.__A = 0
+
+    @property
+    def W(self):
+        '''Getter of weight value'''
+        return self.__W
+
+    @property
+    def b(self):
+        '''Getter of bias value'''
+        return self.__b
+
+    @property
+    def A(self):
+        '''Getter of activation value'''
+        return self.__A
+
+    def forward_prop(self, X):
+        '''Calculates the forward propagation of the neuron'''
+        mul_Z = (np.matmul(self.__W, X)) + self.__b
+        self.__A = 1 / (1 + np.exp(- mul_Z))
+        return self.__A
+
+    def cost(self, Y, A):
+        '''Logisting regression cost'''
+        m = Y.shape[1]
+        q = 1 - Y
+        cost = (-1 / m) * np.sum(Y * np.log(A) + q * np.log(1.0000001 - A))
+        return cost
