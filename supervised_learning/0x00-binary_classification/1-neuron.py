2b3fced1ec9cb0534056396696ac3f7df6deddf0
PilarPinto <piapintoch@unal.edu.co>
2020-08-04 19:39:20 +0000

    [Fix]:Type conditionof 0

--- supervised_learning/0x00-binary_classification/1-neuron.py
+++ supervised_learning/0x00-binary_classification/1-neuron.py
@@ -9,7 +9,7 @@ class Neuron:
     def __init__(self, nx):
         '''class constructor with features of the neuron'''
 
-        if nx % 1 != 0:
+        if type(nx) is not int:
             raise TypeError('nx must be an integer')
         if nx < 1:
             raise ValueError('nx must be a positive integer')
