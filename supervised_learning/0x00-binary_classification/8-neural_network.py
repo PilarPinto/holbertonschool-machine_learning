ad238539ebb901a8d91d748b65c61405e924af34
PilarPinto <piapintoch@unal.edu.co>
2020-08-05 01:23:08 +0000

    [Feature]:Class definition of neural network

---
+++ supervised_learning/0x00-binary_classification/8-neural_network.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+'''Neural Network file'''
+
+import numpy as np
+
+
+class NeuralNetwork:
+    '''Class Neuronal network definition'''
+    def __init__(self, nx, nodes):
+        '''class constructor with features of the neuronal network'''
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(nodes) is not int:
+            raise TypeError('nodes must be an integer')
+        if nodes < 1:
+            raise ValueError('nodes must be a positive integer')
+
+        self.W1 = np.random.normal(size=(nodes, nx))
+        self.b1 = np.zeros((nodes, 1))
+        self.A1 = 0
+        self.W2 = np.random.normal(size=(1, nodes))
+        self.b2 = 0
+        self.A2 = 0
