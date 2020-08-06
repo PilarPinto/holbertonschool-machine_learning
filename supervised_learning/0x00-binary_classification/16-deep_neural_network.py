c2772a125d336ac777968f14682c50ab56fa9dae
PilarPinto <piapintoch@unal.edu.co>
2020-08-06 18:44:10 +0000

    [Test]: Output with nx to last of the loop

---
+++ supervised_learning/0x00-binary_classification/16-deep_neural_network.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+'''Deep Neural Network file'''
+
+import numpy as np
+
+
+class DeepNeuralNetwork:
+    '''neural network performing binary classification:'''
+    def __init__(self, nx, layers):
+        '''class constructor with features of the neuron'''
+
+        if type(nx) is not int:
+            raise TypeError('nx must be an integer')
+        if nx < 1:
+            raise ValueError('nx must be a positive integer')
+        if type(layers) is not list or len(layers) is 0:
+            raise TypeError('layers must be a list of positive integers')
+
+        self.layers = layers
+        self.L = len(layers)
+        self.cache = {}
+        self.weights = {}
+
+        for l in range(len(layers)):
+            if layers[l] < 0:
+                raise ValueError('layers must be a list of positive integers')
+
+            he_al = np.random.randn(layers[l], nx) * np.sqrt(2 / nx)
+            self.weights['W' + str(l+1)] = he_al
+            self.weights['b' + str(l+1)] = np.zeros((layers[l], 1))
+            nx = layers[l]
