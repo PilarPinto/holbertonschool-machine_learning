17faecdb834c78953f1cb856ec14383537dacb95
PilarPinto <piapintoch@unal.edu.co>
2020-08-11 16:45:57 +0000

    [Feature]:Hot encode function

---
+++ supervised_learning/0x01-multiclass_classification/0-one_hot_encode.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+'''one-hot encode file'''
+import numpy as np
+
+
+def one_hot_encode(Y, classes):
+    '''one hot encode definition'''
+    if type(Y) is not np.ndarray:
+        return None
+    if type(classes) is not int:
+        return None
+
+    h_encode = np.zeros((classes, Y.shape[0]))
+    h_encode[Y, np.arange(Y.size)] = 1
+    return h_encode
