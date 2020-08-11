8c0b9a24ac5a0a53a217a25731896498f77d3c1c
PilarPinto <piapintoch@unal.edu.co>
2020-08-11 19:13:12 +0000

    [Test]:Adding condtions

--- supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
+++ supervised_learning/0x01-multiclass_classification/1-one_hot_decode.py
@@ -7,4 +7,8 @@ def one_hot_decode(one_hot):
     '''one hot decode definition'''
     if type(one_hot) is not np.ndarray:
         return None
+    if one_hot.ndim is not 2:
+        return None
+    if 0 not in one_hot and 1 not in one_hot:
+        return None
     return np.argmax(one_hot, axis=0)
