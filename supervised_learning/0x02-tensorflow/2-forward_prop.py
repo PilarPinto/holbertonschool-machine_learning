ad67bc445433675d7af26ae4f29bf5a2108a9dc2
PilarPinto <piapintoch@unal.edu.co>
2020-08-15 21:11:43 +0000

    [Feature]:Create a foward propagation with tf

---
+++ supervised_learning/0x02-tensorflow/2-forward_prop.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Foward propagation'''
+
+import tensorflow as tf
+
+create_layer = __import__('1-create_layer').create_layer
+
+
+def forward_prop(x, layer_sizes=[], activations=[]):
+    '''Foward propagation'''
+    new_layer = create_layer(x, layer_sizes[0], activations[0])
+    for ind in range(1, len(layer_sizes)):
+        new_layer = create_layer(x, layer_sizes[ind], activations[ind])
+    return new_layer
