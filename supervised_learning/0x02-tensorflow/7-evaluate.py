3e64deba80c677c04f656dcaa00bf9a1813717e8
PilarPinto <piapintoch@unal.edu.co>
2020-08-16 04:22:03 +0000

    [Feature]-Evaluate tensorflow

--- supervised_learning/0x02-tensorflow/7-evaluate.py
+++ supervised_learning/0x02-tensorflow/7-evaluate.py
@@ -6,7 +6,7 @@ import tensorflow as tf
 def evaluate(X, Y, save_path):
     '''valuates the output of a neural network'''
     with tf.Session() as sess:
-        saver = tf.train.import_meta_graph(save_path +'.meta')
+        saver = tf.train.import_meta_graph(save_path + '.meta')
         saver.restore(sess, save_path)
 
         x = tf.get_collection("x")[0]
@@ -15,6 +15,6 @@ def evaluate(X, Y, save_path):
         loss = tf.get_collection("loss")[0]
         accuracy = tf.get_collection("accuracy")[0]
 
-        pre, accu, los= sess.run([y_pred, accuracy, loss], feed_dict={
+        pre, accu, los = sess.run([y_pred, accuracy, loss], feed_dict={
             x: X, y: Y})
         return pre, accu, los
