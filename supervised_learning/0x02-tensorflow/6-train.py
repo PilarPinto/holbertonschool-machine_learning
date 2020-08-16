a07376277cec0a3c389c1898164fff468d9ecd02
PilarPinto <piapintoch@unal.edu.co>
2020-08-16 02:38:13 +0000

    [Test]:Adding save_path

--- supervised_learning/0x02-tensorflow/6-train.py
+++ supervised_learning/0x02-tensorflow/6-train.py
@@ -47,5 +47,5 @@ def train(X_train, Y_train, X_valid, Y_valid, layer_sizes, activations,
                 print('\tValidation Cost: {}'.format(v_cost))
                 print('\tValidation Accuracy: {}'.format(v_accur))
             sess.run(train_op, feed_dict={x: X_train, y: Y_train})
-        path = saver.save(sess, save_path)
+        save_path = saver.save(sess, save_path)
     return path
