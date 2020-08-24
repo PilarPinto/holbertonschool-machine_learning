55069c49d6b80980c60507c97083497c1423dad4
PilarPinto <piapintoch@unal.edu.co>
2020-08-24 20:58:45 +0000

    [Feature]:Specificity formula

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -6,7 +6,7 @@ import numpy as np
 def specificity(confusion):
     '''specificity definition'''
     tp = np.diagonal(confusion)
-    fp = np.sum(confusion, axis=1) - tp
+    fp = np.sum(confusion, axis=0) - tp
     fn = np.sum(confusion, axis=1) - tp
     tn = np.sum(confusion) - (fp + fn + tp)
     tnr = tn/(tn+fp)
