314fed887114396f95435724082b13465d77a65a
PilarPinto <piapintoch@unal.edu.co>
2020-08-24 01:24:13 +0000

    [Test]: Syn return

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -7,4 +7,4 @@ def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     n_moment = (beta2 * s) + ((1 - beta2) * grad**2)
     s_m = grad / (np.sqrt(n_moment) + epsilon)
     up_var = var - (alpha * s_m)
-    return up_var, n_moment
+    return(up_var, n_moment)
