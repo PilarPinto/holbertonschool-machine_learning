38b460fbaf6e59c67b7de16d078f9c6fb528cc8f
PilarPinto <piapintoch@unal.edu.co>
2020-08-24 01:22:14 +0000

    [Fix]:Delete a param

--- supervised_learning/0x03-optimization/6-momentum.py
+++ supervised_learning/0x03-optimization/6-momentum.py
@@ -5,6 +5,6 @@ import tensorflow as tf
 
 def create_momentum_op(loss, alpha, beta1):
     '''Upgrade the momentum definition'''
-    optimizer = tf.train.MomentumOptimizer(alpha, beta1).minimize(
-        loss, global_step=batch)
+    optimizer = tf.train.MomentumOptimizer(alpha,
+                                           beta1).minimize(loss)
     return optimizer
