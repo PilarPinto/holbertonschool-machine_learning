d51144206704322c4c217b78435a22d36b7dfae4
PilarPinto <piapintoch@unal.edu.co>
2020-08-24 01:57:20 +0000

    [Feature]:Batch normalization tf

---
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+'''Normalization upgraded file'''
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    '''Normalization upgraded with tf'''
+    ker_i = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.Dense(units=n, kernel_initializer='ker_i').apply(prev)
+
+    mean, var = tf.nn.moments(layer, axes=[0])
+    beta = tf.Variable(tf.constant(0.0, shape=[n]), True)
+    gamma = tf.Variable(tf.constant(1.0, shape=[n]), True)
+
+    b_norm = tf.nn.batch_normalization(layer, mean, var,
+                                       beta, gamma, 1e-8)
+
+    return(activation(b_norm))
