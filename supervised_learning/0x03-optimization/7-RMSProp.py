099b05a8c4ca023d2fa9ef072fc962452763478c
PilarPinto <piapintoch@unal.edu.co>
2020-08-24 01:25:07 +0000

    [Test]:Sqrt math

--- supervised_learning/0x03-optimization/7-RMSProp.py
+++ supervised_learning/0x03-optimization/7-RMSProp.py
@@ -5,6 +5,6 @@
 def update_variables_RMSProp(alpha, beta2, epsilon, var, grad, s):
     '''Update RSM'''
     n_moment = (beta2 * s) + ((1 - beta2) * grad**2)
-    s_m = grad / (np.sqrt(n_moment) + epsilon)
+    s_m = grad / ((n_moment**0.5) + epsilon)
     up_var = var - (alpha * s_m)
     return(up_var, n_moment)
