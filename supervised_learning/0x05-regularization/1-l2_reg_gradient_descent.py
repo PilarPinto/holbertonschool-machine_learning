e26ae0e9e9f310a3aa7a428f4d655f3a00358d10
PilarPinto <piapintoch@unal.edu.co>
2020-08-28 18:35:35 +0000

    [Fix]: Variable derivate and reorganice

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -7,29 +7,21 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     '''Update w and b of NN with gradient descent'''
 
     Z = cache['A' + str(L)]
-    dz = Z - Y
+    dZ = Z - Y
     m = Y.shape[1]
 
     for layer in reversed(range(1, L + 1)):
-        weight_k = 'W' + str(layer)
-        bias_k = 'b' + str(layer)
-        Z_k = 'A' + str(layer)
-        Z1_k = 'A' + str(layer - 1)
-
-        Z = cache[Z_k]
+        Z = cache['A' + str(layer)]
         dz2 = 1 - (Z**2)
-
         if layer == L:
-            dZl = dz
+            dZ = dZ
         else:
-            dZl = dz * dz2
+            dZ = dZ * dz2
 
-        weight = weights[weight_k]
-        dz1 = cache[Z1_k]
-
-        d_weight = (1/m) * np.matmul(dZl, dz1.T) + ((lambtha/m)*weight)
-        d_bias = (1/m) * np.sum(dZl, axis=1, keepdims=True)
-        dz = np.matmul(weight.T, dZl)
-
-        weights[weight_k] = weights[weight_k] - alpha + d_weight
-        weights[bias_k] = weights[bias_k] - alpha + d_bias
+        Wgt = weights['W' + str(layer)]
+        Z1 = cache['A' + str(layer - 1)]
+        dWgt = (1 / m) * np.matmul(dZ, Z1.T) + ((lambtha / m) * Wgt)
+        db = (1 / m) * np.sum(dZ, axis=1, keepdims=True)
+        dZ = np.matmul(Wgt.T, dZ)
+        weights['W' + str(layer)] = weights['W' + str(layer)] - alpha * dWgt
+        weights['b' + str(layer)] = weights['b' + str(layer)] - alpha * db
