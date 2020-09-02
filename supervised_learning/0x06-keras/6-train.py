62ffedc161ba07c6c5a45eced5df1bf74510371e
PilarPinto <piapintoch@unal.edu.co>
2020-09-02 02:27:18 +0000

    [Feature]: trains a model using mini-batch gradient descent, validate and early stopping

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+'''mini-batch gradient descent train file'''
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    '''Trains a model using mini-batch gradient descent
+    early stopping and validate it'''
+    call_lst = []
+    if validation_data:
+        e_stop = K.callbacks.EarlyStopping(monitor='val_loss',
+                                           patience=patience)
+        call_lst.append(e_stop)
+    history = network.fit(x=data, y=labels, epochs=epochs,
+                          batch_size=batch_size,
+                          validation_data=validation_data,
+                          shuffle=shuffle, verbose=verbose)
+
+    return history
