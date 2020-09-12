1f3a37e00343810c3acd87c1942b1cb563034c1c
PilarPinto <piapintoch@unal.edu.co>
2020-09-12 04:26:29 +0000

    [Test]

--- supervised_learning/0x07-cnn/4-lenet5.py
+++ supervised_learning/0x07-cnn/4-lenet5.py
@@ -1,50 +1,48 @@
-#!/usr/bin/env ptyhon3
+#!/usr/bin/env python3
 ''' builds a modified version of the LeNet-5 architecture File'''
 import tensorflow as tf
 
 
 def lenet5(x, y):
-    '''
-    builds a modified version of the
-    LeNet-5 architecture using tensorflow
-    '''
-    ker_init = tf.contrib.layers.variance_scaling_initializer()
+    '''LeNet-5 architecture using tensorflow'''
+
+    k_nit = tf.contrib.layers.variance_scaling_initializer()
     activation = tf.nn.relu
 
-    conv_lay1 = tf.layers.Conv2D(filters=6, kernel_size=5,
-                                 padding='same', activation=activation,
-                                 kernel_initializer=ker_init)(x)
+    layer_1 = tf.layers.Conv2D(filters=6, kernel_size=5, padding='same',
+                               activation=activation,
+                               kernel_initializer=k_init)(x)
 
-    po1 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                 strides=2)(conv_lay1)
+    pool_1 = tf.layers.MaxPooling2D(pool_size=[2, 2],
+                                    strides=2)(layer_1)
 
-    conv_lay2 = tf.layers.Conv2D(filters=16, kernel_size=5,
-                                 padding='valid',
-                                 activation=activation,
-                                 kernel_initializer=ker_init)(po1)
+    layer_2 = tf.layers.Conv2D(filters=16, kernel_size=5,
+                               padding='valid',
+                               activation=activation,
+                               kernel_initializer=k_init)(pool_1)
 
-    po2 = tf.layers.MaxPooling2D(pool_size=[2, 2],
-                                 strides=2)(conv_lay2)
+    pool_2 = tf.layers.MaxPooling2D(pool_size=[2, 2],
+                                    strides=2)(layer_2)
 
-    flatt = tf.layers.Flatten()(po2)
+    flatten = tf.layers.Flatten()(pool_2)
 
-    conv_lay3 = tf.layers.Dense(units=120, activation=activation,
-                                kernel_initializer=ker_init)(flatt)
+    layer_3 = tf.layers.Dense(units=120, activation=activation,
+                              kernel_initializer=k_init)(flatten)
 
-    conv_lay4 = tf.layers.Dense(units=84, activation=activation,
-                                kernel_initializer=ker_init)(conv_lay3)
+    layer_4 = tf.layers.Dense(units=84, activation=activation,
+                              kernel_initializer=k_init)(layer_3)
 
-    out_lay = tf.layers.Dense(units=10,
-                              kernel_initializer=ker_init)(conv_lay4)
+    output_layer = tf.layers.Dense(units=10,
+                                   kernel_initializer=k_init)(layer_4)
 
-    out = tf.nn.softmax(out_lay)
+    y_out = tf.nn.softmax(output_layer)
 
-    loss = tf.losses.softmax_cross_entropy(y, out_lay)
+    loss = tf.losses.softmax_cross_entropy(y, output_layer)
 
-    optimizer = tf.train.AdamOptimizer().minimize(loss)
+    train = tf.train.AdamOptimizer().minimize(loss)
 
-    predict = tf.equal(tf.argmax(y, axis=1),
-                       tf.argmax(out_lay, axis=1))
-    accur = tf.reduce_mean(tf.cast(predict, tf.float32))
+    equality = tf.equal(tf.argmax(y, axis=1),
+                        tf.argmax(output_layer, axis=1))
+    acc = tf.reduce_mean(tf.cast(equality, tf.float32))
 
-    return out, optimizer, loss, accur
+    return y_out, train, loss, acc
