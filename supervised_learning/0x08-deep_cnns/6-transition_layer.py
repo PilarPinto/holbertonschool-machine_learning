797f96de5592cdaf235d34ae9c61c6abea82b3b2
PilarPinto <piapintoch@unal.edu.co>
2020-09-18 04:21:13 +0000

    [Feature]:builds the DenseNet-121

--- supervised_learning/0x08-deep_cnns/6-transition_layer.py
+++ supervised_learning/0x08-deep_cnns/6-transition_layer.py
@@ -1,26 +1,19 @@
 #!/usr/bin/env python3
-'''builds a dense block File'''
+'''File to build a transition layer '''
 import tensorflow.keras as K
 
 
-def dense_block(X, nb_filters, growth_rate, layers):
-    '''builds a dense block '''
+def transition_layer(X, nb_filters, compression):
+    '''builds a transition layer '''
     kernel_init = K.initializers.he_normal(seed=None)
+    filters = int(nb_filters * compression)
 
-    for layer in range(layers):
-        batch1 = K.layers.BatchNormalization()(X)
-        act1 = K.layers.Activation('relu')(batch1)
-        conv1x1 = K.layers.Conv2D(filters=4*growth_rate, kernel_size=1,
-                                  padding='same',
-                                  kernel_initializer=kernel_init)(act1)
+    batch1 = K.layers.BatchNormalization()(X)
+    act1 = K.layers.Activation('relu')(batch1)
+    trans = K.layers.Conv2D(filters=filters, kernel_size=1, padding='same',
+                            kernel_initializer=kernel_init)(act1)
 
-        batch2 = K.layers.BatchNormalization()(conv1x1)
-        act2 = K.layers.Activation('relu')(batch2)
-        conv33 = K.layers.Conv2D(filters=growth_rate, kernel_size=3,
-                                 padding='same',
-                                 kernel_initializer=kernel_init)(act2)
+    avg_p = K.layers.AveragePooling2D(pool_size=(2, 2), strides=(2, 2),
+                                      padding='same')(trans)
 
-        X = K.layers.concatenate([X, conv33])
-        nb_filters += growth_rate
-
-    return X, nb_filters
+    return avg_p, filters
